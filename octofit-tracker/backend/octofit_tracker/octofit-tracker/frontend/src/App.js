ab1926653e7c80de16ca6e2e15b112098f22643e
Aryan Malik <aryanmalik.ec@gmail.com>
2025-12-26 12:43:28 +0000

    step5

--- octofit-tracker/backend/octofit_tracker/octofit-tracker/frontend/src/App.js
+++ octofit-tracker/backend/octofit_tracker/octofit-tracker/frontend/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import Activities from './components/Activities';
 import Leaderboard from './components/Leaderboard';
 import Teams from './components/Teams';
@@ -11,15 +11,18 @@ function App() {
   return (
     <Router>
       <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
-          <Link className="navbar-brand" to="/">Octofit Tracker</Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item"><Link className="nav-link" to="/activities">Activities</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/leaderboard">Leaderboard</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/teams">Teams</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/users">Users</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/workouts">Workouts</Link></li>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+          <NavLink className="navbar-brand" to="/">Octofit Tracker</NavLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item"><NavLink className="nav-link" to="/activities">Activities</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/leaderboard">Leaderboard</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/teams">Teams</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/users">Users</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/workouts">Workouts</NavLink></li>
             </ul>
           </div>
         </nav>
@@ -29,7 +32,7 @@ function App() {
           <Route path="/teams" element={<Teams />} />
           <Route path="/users" element={<Users />} />
           <Route path="/workouts" element={<Workouts />} />
-          <Route path="/" element={<h2>Welcome to Octofit Tracker!</h2>} />
+          <Route path="/" element={<div className="jumbotron text-center py-5"><h1 className="display-4">Welcome to Octofit Tracker!</h1><p className="lead">Track your fitness, join teams, and climb the leaderboard!</p></div>} />
         </Routes>
       </div>
     </Router>
